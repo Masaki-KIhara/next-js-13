b66f272b3537340f68255ede405ef47d1c13a46c
Masaki Kihara <masaki1224.1998@gmail.com>
2024-01-03 21:21:03 +0900

    app直下のpageに作成したファイルをimport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { Suspense } from 'react'
 import NotesList from './components/notes-list'
+import RefreshButton from './components/refresh-button'
+import Spinner from './components/spinner'
+import TimerCounter from './components/timer-counter'
 
 export default function Home() {
   return (
     <div className="m-10 text-center">
       <p>Hello world</p>
-      {/* @ts-expect-error */}
-      <NotesList />
+      <Suspense fallback={<Spinner color="border-green-500" />}>
+        {/* @ts-expect-error */}
+        <NotesList />
+      </Suspense>
+      <TimerCounter />
+      <RefreshButton />
     </div>
   )
 }
